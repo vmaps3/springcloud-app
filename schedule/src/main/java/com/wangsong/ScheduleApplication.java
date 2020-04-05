fcb11411d5e3e46b6af5423f1f1bff5e8f277f12
wangsong <vmaps@qq.com>
2020-04-05 17:38:06 +0800

    修复断路器监控，优化名称

--- schedule/src/main/java/com/wangsong/ScheduleApplication.java
+++ schedule/src/main/java/com/wangsong/ScheduleApplication.java
@@ -5,10 +5,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication

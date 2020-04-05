879ada54693dcd364138598c2fca8d9180641028
wangsong <vmaps@qq.com>
2020-04-05 17:09:50 +0800

    修复断路器监控，优化名称

---
+++ hystrix-dashboard/src/main/java/com/wangsong/HystrixDashboardApplication.java
@@ -0,0 +1,15 @@
+package com.wangsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard
+@SpringCloudApplication
+@EnableDiscoveryClient
+public class HystrixDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+}
